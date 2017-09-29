64681e4a40d548f480eb5f46baabb074bbb150d5
Kation <kation@outlook.com>
2017-09-29 14:47:40 +0800

    添加领域扩展

---
+++ samples/Wodsoft.Forum.Sample.Domain/ForumDomainExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wodsoft.ComBoost;
+using Wodsoft.ComBoost.Data.Entity;
+
+namespace Wodsoft.Forum.Sample.Domain
+{
+    public class ForumDomainExtension : DomainExtension
+    {
+        public override void OnInitialize(IServiceProvider serviceProvider, IDomainService domainService)
+        {
+            ForumDomainService domain = (ForumDomainService)domainService;
+            domain.ForumQuery += Domain_ForumQuery;
+        }
+
+        private Task Domain_ForumQuery(IDomainExecutionContext context, ComBoost.Data.EntityQueryEventArgs<IForum> e)
+        {
+            e.Queryable = e.Queryable.Unwrap<IForum, Entity.Forum>().OrderBy(t => t.Order).Wrap<IForum, Entity.Forum>();
+            return Task.CompletedTask;
+        }
+    }
+}
